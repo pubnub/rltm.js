eea0dc3e5584eccc76549f4a9313c0e34badcc4a
Ian Jennings <ian@meetjennings.com>
2016-10-10 19:21:27 -0500

    Move to browserify style so this works on backend (#11)
    * fix #9 and make gulp output to a special /web directory
    
    * install pubnub, ignore node_modules
    
    * webpack test working
    
    * move to a browserify workflow so this will work on backend as well fix #9, fix #8, fix #3, fix #5, fix #6
    
    * remove gulp concat from repo

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 const gulp = require('gulp');
-const concat = require("gulp-concat");
- 
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+
 // task
-gulp.task('minify-js', function () {
-    gulp
-    .src(['./bower_components/pubnub/dist/web/pubnub.js', './src/index.js'])
-    .pipe(concat('realtime.js'))
-    .pipe(gulp.dest('./dist'));
+gulp.task('compile', function () {
+    
+    browserify({
+        entries: 'src/index.js',
+        debug: true
+    })
+    .bundle()
+    .pipe(source('rltm.js'))
+    .pipe(gulp.dest('./web/'));
+
 });
 
-gulp.task('default', ['minify-js']);
+gulp.task('default', ['compile']);
